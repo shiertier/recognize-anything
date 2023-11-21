ba8473a74a2bcc4c4c8c8bec697a25b3a426e4ed
shiertier <90256303+shiertier@users.noreply.github.com>
2023-11-21 21:05:37 +0800

    Update 12t.py

--- 12t.py
+++ 12t.py
@@ -10,10 +10,12 @@ from pathlib import Path
 from ram import get_transform
 from ram.models import ram_plus
 from ram import inference_ram_openset as inference
+import concurrent.futures
 from concurrent.futures import ThreadPoolExecutor
 from torchvision.transforms import Normalize, Compose, Resize, ToTensor
 from ram.utils import build_openset_llm_label_embedding
 from torch import nn
+
 
 parser = argparse.ArgumentParser(
     description='识别万物———+————反推提示词')
@@ -58,6 +60,16 @@ def get_gpu_lock_memory(gpu_index=0):
     lock_memory = min(round(max_memory * 0.8), round(max_memory) - 1) # 取整到最接近的整数
     return lock_memory
 
+def get_gpu_use_memory(gpu_index=0):
+    pynvml.nvmlInit()
+    device_count = pynvml.nvmlDeviceGetCount()
+    for i in range(device_count):
+        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+        meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        use_memory = meminfo.used / 1024**3
+    pynvml.nvmlShutdown()
+    return use_memory
+
 def glob_images_pathlib(dir_path):
     image_paths = []
 
@@ -97,38 +109,77 @@ def check_model_exist():
         os.rmdir(os.path.join(parent_dir, model_name))
 
 def transform_and_inference(image_path, model, transform, device):
-
     try:
         image_res = transform(Image.open(image_path)).unsqueeze(0).to(device)
         res = inference(image_res, model)
-        tag = res[0]
+        tag = res.replace(" | ", ", ")
+        del image_res
         return tag
     except Exception as e:
         print(f"错误！{image_path}: {e}")
         return None
 
-def process_single_image(image_path, model, transform, device):
-    txt_path = os.path.splitext(image_path)[0] + '.txt'
+def transform_and_inference_2(image_path, model, transform, device):
+    try:
+        image_res = transform(Image.open(image_path)).unsqueeze(0).to(device)
+        res = inference(image_res, model)
+        tag = res.replace(" | ", ", ")
+        del image_res
+        return tag
+    except Exception as e:
+        print(f"错误！{image_path}: {e}")
+        return None
 
+def process_single_image(image_path, model, transform, device, houzhui):
+    txt_path = os.path.splitext(image_path)[0] + '.' + houzhui
     if os.path.exists(txt_path):
         print(f"{image_path}已有标签.")
         return
-    tag = transform_and_inference(image_path, model, transform, device)
+    if houzhui == "txt1":
+        tag = transform_and_inference(image_path, model, transform, device)
+    else :
+        tag = transform_and_inference_2(image_path, model, transform, device)
     if tag is not None:
         with open(txt_path, 'w') as txt_file:
-            txt_file.write(tag.replace(" | ", ", "))
+            txt_file.write(tag)
 
-def process_images_in_directory(dir_path, model, transform, device, lock_memory):
+def process_images_in_directory(dir_path, model, transform, device, lock_memory, houzhui):
     image_paths = glob_images_pathlib(dir_path)
-    print(image_paths)
-
     with ThreadPoolExecutor() as executor:
         for image_path in image_paths:
-            if gpu.memoryUsed < lock_memory:
-                executor.submit(process_single_image, image_path, model, transform, device)
+            use_memory = get_gpu_use_memory()
+            if use_memory < lock_memory:
+                executor.submit(process_single_image, image_path, model, transform, device, houzhui)
             else:
                 time.sleep(5)
+        executor.shutdown(wait=True)
+        completed_tasks, _ = concurrent.futures.wait(executor._threads, return_when=concurrent.futures.ALL_COMPLETED)
+        del model
     print("所有图像已提交处理。")
+
+def model_1(image_dir, transform, device, lock_memory):
+    print("正在将模型1装载到cuda...")
+    model = ram_plus(pretrained=args.pretrained, image_size=args.image_size, vit='swin_l', threshold=args.threshold)
+    model.eval()
+    model = model.to(device)
+    print("开始反推1")
+    process_images_in_directory(image_dir, model, transform, device, lock_memory, "txt1")
+
+def model_2(image_dir, transform, device, lock_memory):
+    print("正在将模型2装载到cuda...")
+    model = ram_plus(pretrained=args.pretrained, image_size=args.image_size, vit='swin_l', threshold=args.threshold)
+    print('Building tag embedding:')
+    with open(args.llm_tag_des, 'rb') as fo:
+        llm_tag_des = json.load(fo)
+    openset_label_embedding, openset_categories = build_openset_llm_label_embedding(llm_tag_des)
+    model.tag_list = np.array(openset_categories)
+    model.label_embed = nn.Parameter(openset_label_embedding.float())
+    model.num_class = len(openset_categories)
+    model.class_threshold = torch.ones(model.num_class) * 0.5
+    model.eval()
+    model = model.to(device)
+    print("开始反推2")
+    process_images_in_directory(image_dir, model, transform, device, lock_memory, "txt2")   
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -144,19 +195,6 @@ if __name__ == "__main__":
     transform = get_transform(image_size=args.image_size)
     print("配置transform成功")
     check_model_exist()
-    print("正在将模型装载到cuda...")
-    model = ram_plus(pretrained=args.pretrained, image_size=args.image_size, vit='swin_l', threshold=args.threshold)
-    #######set openset interference
-    print('Building tag embedding:')
-    with open(args.llm_tag_des, 'rb') as fo:
-        llm_tag_des = json.load(fo)
-    openset_label_embedding, openset_categories = build_openset_llm_label_embedding(llm_tag_des)
-    model.num_class = len(openset_categories)
-    model.class_threshold = torch.ones(model.num_class) * 0.5
-    
-    model.eval()
-    model = model.to(device)
-    print("模型装载到cuda成功")
-    print("开始反推")
     image_dir = Path(args.image_dir)
-    process_images_in_directory(image_dir, model, transform, device, lock_memory)
+    model_1(image_dir, transform, device, lock_memory)
+    model_2(image_dir, transform, device, lock_memory)
